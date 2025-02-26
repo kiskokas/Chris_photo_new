2dfc2e28eb39ee080698dc298a115e4362c9eefa
kiskokas <kiskokas90@gmail.com>
2025-02-26 07:20:53 +0100

    animation of hamburger menu

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,6 +2,7 @@
 
 import { useState, useEffect } from "react";
 import Link from "next/link";
+import { motion, AnimatePresence } from "framer-motion";
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -28,30 +29,20 @@ const Header = () => {
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
-    setIsDropdownOpen(false); // Ensure dropdown closes when toggling main menu
+    setIsDropdownOpen(false);
   };
 
   const toggleDropdown = () => {
     setIsDropdownOpen(!isDropdownOpen);
   };
 
-  const closeMenu = () => {
-    setIsOpen(false);
-  };
-
   return (
     <header className="bg-cyan-300 text-cyan-700 p-5 flex justify-between items-center fixed w-full top-0 z-50">
       <h1 className="text-2xl">Chris Photo</h1>
-      <nav className={`hidden md:flex space-x-4 ${isOpen ? "hidden" : "flex"}`}>
-        <Link href="/" className="hover:underline" onClick={closeMenu}>
-          Kezdőoldal
-        </Link>
-        <a href="#about" className="hover:underline" onClick={closeMenu}>
-          Rólam
-        </a>
-        <a href="#prices" className="hover:underline" onClick={closeMenu}>
-          Csomagok és árak
-        </a>
+      <nav className="hidden md:flex space-x-4">
+        <Link href="/" className="hover:underline">Kezdőoldal</Link>
+        <a href="#about" className="hover:underline">Rólam</a>
+        <a href="#prices" className="hover:underline">Csomagok és árak</a>
         <div className="relative inline-block text-left">
           <button 
             onClick={toggleDropdown}
@@ -61,64 +52,64 @@ const Header = () => {
           {isDropdownOpen && (
             <div className="absolute right-0 mt-2 w-48 bg-white dark:bg-cyan-300 rounded-md shadow-lg z-50">
               <div className="py-1" role="menu">
-                <Link href="/gallery#portrait" className="block px-4 py-2 text-sm text-gray-700 dark:text-white hover:bg-gray-100 dark:hover:bg-gray-600" onClick={closeMenu}>
+                <Link href="/gallery#portrait" className="block px-4 py-2 text-sm text-gray-700 dark:text-white hover:bg-gray-100 dark:hover:bg-gray-600">
                   Portré
                 </Link>
-                <Link href="/gallery#family" className="block px-4 py-2 text-sm text-gray-700 dark:text-white hover:bg-gray-100 dark:hover:bg-gray-600" onClick={closeMenu}>
+                <Link href="/gallery#family" className="block px-4 py-2 text-sm text-gray-700 dark:text-white hover:bg-gray-100 dark:hover:bg-gray-600">
                   Családi
                 </Link>
-                <Link href="/gallery#child" className="block px-4 py-2 text-sm text-gray-700 dark:text-white hover:bg-gray-100 dark:hover:bg-gray-600" onClick={closeMenu}>
+                <Link href="/gallery#child" className="block px-4 py-2 text-sm text-gray-700 dark:text-white hover:bg-gray-100 dark:hover:bg-gray-600">
                   Gyerek
                 </Link>
-                <Link href="/gallery#nature" className="block px-4 py-2 text-sm text-gray-700 dark:text-white hover:bg-gray-100 dark:hover:bg-gray-600" onClick={closeMenu}>
+                <Link href="/gallery#nature" className="block px-4 py-2 text-sm text-gray-700 dark:text-white hover:bg-gray-100 dark:hover:bg-gray-600">
                   Természet
                 </Link>
               </div>
             </div>
           )}
         </div>
-        <a href="#contact" className="hover:underline" onClick={closeMenu}>
-          Kapcsolat
-        </a>
+        <a href="#contact" className="hover:underline">Kapcsolat</a>
       </nav>
       <div className="md:hidden">
         <button onClick={toggleMenu} className="focus:outline-none">
-          <span className="text-2xl">☰</span>
+          <motion.div animate={{ rotate: isOpen ? 180 : 0 }} className="text-2xl">
+            {isOpen ? "✖" : "☰"}
+          </motion.div>
         </button>
       </div>
-      {isOpen && (
-        <div className="absolute top-16 left-2rem right-0 bg-cyan-300 text-cyan-700 flex py-2 flex-col z-50">
-          <Link href="/" className="py-0.5" onClick={closeMenu}>
-            Kezdőoldal
-          </Link>
-          <a href="#about" className="py-0.5" onClick={closeMenu}>
-            Rólam
-          </a>
-          <a href="#prices" className="py-0.5" onClick={closeMenu}>
-            Csomagok és árak
-          </a>
-          <div className="py-2">
-            <span className="font-bold">Galéria</span>
-            <div className="bg-cyan-50 text-cyan-700 rounded-md shadow-lg w-full">
-              <Link href="/gallery#portrait" className="block px-4 py-1 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600" onClick={closeMenu}>
-                Portré
-              </Link>
-              <Link href="/gallery#family" className="block px-4 py-1 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600" onClick={closeMenu}>
-                Családi
-              </Link>
-              <Link href="/gallery#child" className="block px-4 py-1 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600" onClick={closeMenu}>
-                Gyerek
-              </Link>
-              <Link href="/gallery#nature" className="block px-4 py-1 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600" onClick={closeMenu}>
-                Természet
-              </Link>
+      <AnimatePresence>
+        {isOpen && (
+          <motion.div
+            initial={{ y: -50, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            exit={{ y: -50, opacity: 0 }}
+            transition={{ duration: 0.3 }}
+            className="absolute top-16 left-0 right-0 bg-cyan-300 text-cyan-700 flex flex-col py-1 z-50 shadow-lg"
+          >
+            <Link href="/" className="py-1 text-center" onClick={toggleMenu}>Kezdőoldal</Link>
+            <a href="#about" className="py-1 text-center" onClick={toggleMenu}>Rólam</a>
+            <a href="#prices" className="py-1 text-center" onClick={toggleMenu}>Csomagok és árak</a>
+            <div className="py-2 text-center">
+              <span className="font-bold">Galéria</span>
+              <div className="bg-cyan-50 text-cyan-700 rounded-md shadow-lg w-full">
+                <Link href="/gallery#portrait" className="block px-4 py-1 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600" onClick={toggleMenu}>
+                  Portré
+                </Link>
+                <Link href="/gallery#family" className="block px-4 py-1 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600" onClick={toggleMenu}>
+                  Családi
+                </Link>
+                <Link href="/gallery#child" className="block px-4 py-1 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600" onClick={toggleMenu}>
+                  Gyerek
+                </Link>
+                <Link href="/gallery#nature" className="block px-4 py-1 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600" onClick={toggleMenu}>
+                  Természet
+                </Link>
+              </div>
             </div>
-          </div>
-          <a href="#contact" className="py-0.5" onClick={closeMenu}>
-            Kapcsolat
-          </a>
-        </div>
-      )}
+            <a href="#contact" className="py-1 text-center" onClick={toggleMenu}>Kapcsolat</a>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </header>
   );
 };
