a6a7e8463d58c2ec752848bf3be047f941f0b87f
kiskokas <kiskokas90@gmail.com>
2025-03-06 15:18:30 +0100

    build failure fix

--- app/components/Gallery.tsx
+++ app/components/Gallery.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { motion } from "framer-motion";
-import { useState, useEffect } from "react";
+import { useState} from "react";
 import Image from "next/image";
 import Lightbox from "yet-another-react-lightbox";
 import "yet-another-react-lightbox/styles.css";
